3749c24f7e9acaf3872a05182f53e941ae743a93
puckblocker <servergoalie0423@gmail.com>
2026-04-05 18:10:59 -0500

    Feat: Path Tracer Prep + Helper Functions
    Moved helper functions into own files, created random number generator function and features from whitted ray tracer that wont transfer.

---
+++ src/helper.h
@@ -0,0 +1,23 @@
+// Helper Functions
+#pragma once
+
+#include "intersection.h"
+
+class Help
+{
+public:
+    // Random Number Generator
+    float RandFloat();
+
+    // Fresnel Reflectance
+    glm::vec3 FrsRflct(glm::vec3 normal, glm::vec3 direction, glm::vec3 F0);
+
+    // Normal Distribution Function (Approx relative surface area of microfacets exactly aligned to the halfway vector wh (from learnopengl))
+    float DistrFunc(float alphaSqr, glm::vec3 normal, glm::vec3 wh);
+
+    // Geometry Function (Approx relative surface area where micro surface details overshadow each other, causing light rays to be covered (from opengl))
+    float GeomFunc(float k, glm::vec3 normal, glm::vec3 w0, glm::vec3 wi);
+
+    // PBR BRDF CALCULATIONS (DIRECT LIGHTING)
+    glm::vec3 BRDF(glm::vec3 R, HitInfo hitInfo, glm::vec3 w0, glm::vec3 wi);
+};
--- src/renderer.cpp
+++ src/renderer.cpp
@@ -5,67 +5,6 @@
 #include <glm/gtx/string_cast.hpp>
 #include "omp.h"
 
-// HELPER FUNCTIONS
-// Fresnel Reflectance
-glm::vec3 Renderer::FrsRflct(glm::vec3 normal, glm::vec3 direction, glm::vec3 F0)
-{
-    glm::vec3 Fr = F0 + (1.0f - F0) * glm::pow((1.0f - glm::dot(normal, direction)), 5.0f);
-    return Fr;
-}
-
-// ROUGH SPECULAR (Trowbridge-Reitz)
-// Normal Distribution Function (Approx relative surface area of microfacets exactly aligned to the halfway vector wh (from learnopengl))
-float Renderer::DistrFunc(float alphaSqr, glm::vec3 normal, glm::vec3 wh)
-{
-    float pi = 3.14159265359;
-    float D = alphaSqr / (pi * ((glm::dot(normal, wh) * glm::dot(normal, wh)) * (alphaSqr - 1) + 1));
-    return D;
-}
-
-// Geometry Function (Approx relative surface area where micro surface details overshadow each other, causing light rays to be covered (from opengl))
-float Renderer::GeomFunc(float k, glm::vec3 normal, glm::vec3 w0, glm::vec3 wi)
-{
-    float w0HalfVec = glm::dot(normal, w0) / (glm::dot(normal, w0) * (1 - k) + k);
-    float wiHalfVec = glm::dot(normal, wi) / (glm::dot(normal, wi) * (1 - k) + k);
-    float G = glm::max(w0HalfVec, 0.0f) * glm::max(wiHalfVec, 0.0f); // clamped to prevent negative light (light from behind object)
-    return G;
-}
-
-// PBR BRDF CALCULATIONS (DIRECT LIGHTING)
-glm::vec3 Renderer::BRDF(glm::vec3 R, HitInfo hitInfo, glm::vec3 w0, glm::vec3 wi)
-{
-    float pi = 3.14159265359;
-
-    // Ideal Diffuse
-    glm::vec3 idealDiffuse = R / pi;
-
-    // IDEAL SPECULAR
-    // Fresnel Schlick Approx (Calculate Reflection Based On Incident Angle)
-    glm::vec3 F0 = glm::vec3(0.04f);
-    if (hitInfo.mat.metallic)
-    {
-        F0 = glm::mix(F0, hitInfo.mat.albedo, hitInfo.mat.metallic); // mix the fresnel reflection with the base color based on how metallic object is
-    }
-
-    // ROUGH SPECULAR (Direction Diffuse)
-
-    // Trowbridge Reitz (Normal Distr Function)
-    glm::vec3 wh = glm::normalize(0.5f * (w0 + wi));      // half way vector
-    float alpha = glm::max(hitInfo.mat.roughness, 0.01f); // roughness parameter
-    float alphaSqr = alpha * alpha;
-    float k = (alphaSqr + 1.0f) / 8.0f;
-    float D = DistrFunc(alphaSqr, hitInfo.normal, wh); // how aligned surface normal is with microfacet normals
-    float G = GeomFunc(k, hitInfo.normal, w0, wi);     // how much masking, shadowing, interreflection due to facet distribution
-
-    // Rough Specular BRDF
-    glm::vec3 roughSpec = (D * G * FrsRflct(hitInfo.normal, wh, w0)) / (4.0f * glm::dot(hitInfo.normal, w0) * glm::dot(hitInfo.normal, wi));
-
-    // PBR BRDF RESULT (Direct Lighting)
-    glm::vec3 directLighting = idealDiffuse + roughSpec;
-
-    return directLighting;
-}
-
 // RENDER
 void Renderer::render(float *pixelBuffer, int resWidth, int resHeight)
 {
@@ -83,11 +22,10 @@ void Renderer::render(float *pixelBuffer, int resWidth, int resHeight)
             Ray ray;
             glm::vec3 color(0.0f);
 
-            int smpleAmnt = 8;                   // samples per pixel
-            double smpleScale = 1.0 / smpleAmnt; // color scale factor
+            int smpleAmnt = 8; // samples per pixel
 
-            // Loop For Amount of Samples
-            for (int sample = 0; sample < smpleAmnt; sample++)
+            // Generate Jittered Rays
+            for (int i = 0; i < smpleAmnt; i++)
             {
                 // Generate Ray
                 ray = camera.rayGeneration(i, j);
@@ -95,10 +33,6 @@ void Renderer::render(float *pixelBuffer, int resWidth, int resHeight)
                 // Call Tracer
                 color += tracer(ray, 0);
             }
-            // float r = color.x;
-            // float g = color.y;
-            // float b = color.z;
-            color *= smpleScale;
 
             // Grab RGB Components
             int index = (j * resWidth + i) * 3; // multiply by 3 to account for RGB components and resWidth to prevent overwriting pixels
@@ -159,163 +93,78 @@ glm::vec3 Renderer::tracer(Ray ray, unsigned int depth)
     // Generate Light
     if (hitInfo.valid)
     {
-        // Shadow Variables
-        glm::vec3 toLight; // = pointLight.origin - hitInfo.point;
-        float distToLight; // = glm::length(toLight);
-        glm::vec3 lightRad;
-        bool inShadow = false;
-        float pi = 3.14159265359;
-        glm::vec3 R = hitInfo.mat.albedo;                             // reflectance
-        glm::vec3 w0 = glm::normalize(camera.origin - hitInfo.point); // outgoing direction
+        // // Shadow Variables
+        // glm::vec3 toLight; // = pointLight.origin - hitInfo.point;
+        // float distToLight; // = glm::length(toLight);
+        // glm::vec3 lightRad;
+        // bool inShadow = false;
+        // float pi = 3.14159265359;
+        // glm::vec3 R = hitInfo.mat.albedo;                             // reflectance
+        // glm::vec3 w0 = glm::normalize(camera.origin - hitInfo.point); // outgoing direction
 
-        if (glm::length(directionalLight.color) > 0.0f)
-        {
-            toLight = glm::normalize(-directionalLight.direction);
-            distToLight = 1000000.0f;
-            lightRad = light.directionalLight(directionalLight, hitInfo, w0);
-        }
-        else if (glm::length(pointLight.color) > 0.0f)
-        {
-            toLight = pointLight.origin - hitInfo.point;
-            distToLight = glm::length(toLight);
-            lightRad = light.pointLight(hitInfo.point, pointLight, hitInfo, w0);
-        }
+        // if (glm::length(directionalLight.color) > 0.0f)
+        // {
+        //     toLight = glm::normalize(-directionalLight.direction);
+        //     distToLight = 1000000.0f;
+        //     lightRad = light.directionalLight(directionalLight, hitInfo, w0);
+        // }
+        // else if (glm::length(pointLight.color) > 0.0f)
+        // {
+        //     toLight = pointLight.origin - hitInfo.point;
+        //     distToLight = glm::length(toLight);
+        //     lightRad = light.pointLight(hitInfo.point, pointLight, hitInfo, w0);
+        // }
 
-        glm::vec3 wi = glm::normalize(toLight); // incoming direction
+        // glm::vec3 wi = glm::normalize(toLight); // incoming direction
 
-        // Generate Shadows
-        Ray shadowRay;
-        shadowRay.origin = hitInfo.point + (hitInfo.normal * 0.01f); // offset to avoid self shadowing
-        shadowRay.direction = glm::normalize(toLight);
+        // // Generate Shadows
+        // Ray shadowRay;
+        // shadowRay.origin = hitInfo.point + (hitInfo.normal * 0.01f); // offset to avoid self shadowing
+        // shadowRay.direction = glm::normalize(toLight);
 
-        // Shadow Block Check
-        // Sphere
-        HitInfo shadowHit;
-        for (i = 0; i < spheres.size(); i++)
-        {
-            shadowHit = intersect.intersectSphere(shadowRay, spheres[i], xForms);
-            if (shadowHit.valid && shadowHit.distance < distToLight && hitInfo.objID != shadowHit.objID) // Check for hit and behind object
-                inShadow = true;
-        }
+        // // Shadow Block Check
+        // // Sphere
+        // HitInfo shadowHit;
+        // for (i = 0; i < spheres.size(); i++)
+        // {
+        //     shadowHit = intersect.intersectSphere(shadowRay, spheres[i], xForms);
+        //     if (shadowHit.valid && shadowHit.distance < distToLight && hitInfo.objID != shadowHit.objID) // Check for hit and behind object
+        //         inShadow = true;
+        // }
 
-        // Triangle
-        for (i = 0; i < triangles.size(); i++)
-        {
-            shadowHit = intersect.intersectTriangle(shadowRay, triangles[i], xForms);
-            if (shadowHit.valid && shadowHit.distance < distToLight && hitInfo.objID != shadowHit.objID)
-                inShadow = true;
-        }
+        // // Triangle
+        // for (i = 0; i < triangles.size(); i++)
+        // {
+        //     shadowHit = intersect.intersectTriangle(shadowRay, triangles[i], xForms);
+        //     if (shadowHit.valid && shadowHit.distance < distToLight && hitInfo.objID != shadowHit.objID)
+        //         inShadow = true;
+        // }
 
-        // Plane
-        for (i = 0; i < planes.size(); i++)
-        {
-            shadowHit = intersect.intersectPlane(shadowRay, planes[i], xForms);
-            if (shadowHit.valid && shadowHit.distance < distToLight && hitInfo.objID != shadowHit.objID)
-                inShadow = true;
-        }
+        // // Plane
+        // for (i = 0; i < planes.size(); i++)
+        // {
+        //     shadowHit = intersect.intersectPlane(shadowRay, planes[i], xForms);
+        //     if (shadowHit.valid && shadowHit.distance < distToLight && hitInfo.objID != shadowHit.objID)
+        //         inShadow = true;
+        // }
 
-        // In Shadow
-        if (inShadow)
-        {
-            // Set Color
-            specCoeff = glm::vec3(0.05f) * hitInfo.mat.albedo;
-        }
-        // Not In Shadow
-        else
-        {
-            // PBR BRDF Calculations (DIRECT LIGHTING)
-            glm::vec3 directLighting = BRDF(R, hitInfo, w0, wi);
+        // // In Shadow
+        // if (inShadow)
+        // {
+        //     // Set Color
+        //     specCoeff = glm::vec3(0.05f) * hitInfo.mat.albedo;
+        // }
+        // // Not In Shadow
+        // else
+        // {
+        //     // PBR BRDF Calculations (DIRECT LIGHTING)
+        //     glm::vec3 directLighting = BRDF(R, hitInfo, w0, wi);
 
-            // Add To Color
-            specCoeff = directLighting * lightRad;
-        }
+        //     // Add To Color
+        //     specCoeff = directLighting * lightRad;
+        // }
 
-        color += specCoeff;
-        // WHITTED RAY TRACER
-        glm::vec3 indirectRad;
-
-        // Metallic Surface
-        if (hitInfo.mat.metallic)
-        {
-            // Reflection Ray
-            Ray reflctRay;
-            reflctRay.origin = hitInfo.point + (hitInfo.normal * 0.001f);
-            reflctRay.direction = glm::reflect(ray.direction, hitInfo.normal);
-
-            glm::vec3 reflctColor = glm::vec3(1.0f) * tracer(reflctRay, depth + 1);
-
-            color += (reflctColor * hitInfo.mat.albedo);
-        }
-
-        // Glass Surface
-        else if (hitInfo.mat.ior >= 1.5f)
-        {
-            glm::vec3 attenuation = glm::vec3(1.0f);
-
-            // Reflection Ray
-            Ray reflctRay;
-            glm::vec3 reflctColor = glm::vec3(0.0f);
-
-            // Refraction Ray
-            Ray refrctRay;
-            glm::vec3 refrctColor = glm::vec3(0.0f);
-
-            // Direction Through Snell's Law
-            float ior; // ratio of refract index (air) / refract index (glass)
-            glm::vec3 normal;
-
-            // Outside Object
-            if (glm::dot(ray.direction, hitInfo.normal) < 0.0f)
-            {
-                ior = 1.0f / hitInfo.mat.ior; // air
-                normal = hitInfo.normal;
-            }
-            // Inside Object
-            else
-            {
-                ior = hitInfo.mat.ior;    // glass
-                normal = -hitInfo.normal; // flip normal
-            }
-
-            // Snell's Law Variables
-            float cosTheta = glm::min(glm::dot(-ray.direction, normal), 1.0f); // cosine angle of incidence (how straight on light is hitting surface)
-            float sinTheta = glm::sqrt(1.0f - cosTheta * cosTheta);            // sine angle of incidence (how much light bends)
-
-            // Total Internal Reflection (100% of Light Reflects)
-            if (ior * sinTheta > 1.0f) // only reflect
-            {
-                reflctRay.origin = hitInfo.point + (normal * 0.001f);
-                reflctRay.direction = glm::reflect(ray.direction, normal);
-
-                reflctColor = attenuation * tracer(reflctRay, depth + 1);
-
-                color += reflctColor;
-            }
-            else // refract and reflect
-            {
-                // Send Refraction Ray
-                refrctRay.origin = hitInfo.point - (normal * 0.001f);
-                refrctRay.direction = glm::refract(ray.direction, normal, ior);
-                glm::vec3 refrctRes = attenuation * tracer(refrctRay, depth + 1);
-
-                // Send Reflection Ray (Does Both^)
-                reflctRay.origin = hitInfo.point + (normal * 0.001f);
-                reflctRay.direction = glm::reflect(ray.direction, normal);
-                glm::vec3 reflctRes = tracer(reflctRay, depth + 1);
-
-                // Schlick Approximation For Reflectance Percentage
-                glm::vec3 F0 = glm::vec3(0.04f); // how much light will pass throuh when looking straight at glass
-                glm::vec3 viewDir = -ray.direction;
-                glm::vec3 frsnl = FrsRflct(normal, viewDir, F0); // find percentage of reflection
-
-                // Implement the Reflectance Percentage
-                reflctColor = reflctRes * frsnl;
-                refrctColor = refrctRes * (glm::vec3(1.0f) - frsnl);
-
-                // Combine the Reflection and Refraction
-                color += (refrctColor + reflctColor) * attenuation;
-            }
-        }
+        // color += specCoeff;
 
         return color;
     }
